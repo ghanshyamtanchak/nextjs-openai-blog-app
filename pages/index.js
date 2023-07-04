93b2dcd3bf71632fbc7727e12d47d22fbc375e0f
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-07-04 23:14:19 +0530

    setup-auth0

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>
+        Home page <Link href={"/api/auth/login"}>Login</Link>
+      </h1>
     </div>
   );
 }
